78131157a2918cef8674f2711db5c073c1118600
Ryan-GM <71759184+Ryan-GM@users.noreply.github.com>
2024-08-12 19:45:44 +0300

    update to include handling requests for campsiteId

--- nucampFunctions/routes/campsiteRouter.js
+++ nucampFunctions/routes/campsiteRouter.js
@@ -21,7 +21,6 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
-// New campsiteRouter.route()
 campsiteRouter.route('/:campsiteId')
 .all((req, res, next) => {
     res.statusCode = 200;
@@ -32,12 +31,13 @@ campsiteRouter.route('/:campsiteId')
     res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
 })
 .post((req, res) => {
-    res.statusCode = 403;
-    res.end(`POST operations not supported on /campsites/${req.params.campsiteId}`);
+  res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
 })
 .put((req, res) => {
     res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-    res.end(`Will update the campsite: ${req.body.name} with description: ${req.body.description}`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
 })
 .delete((req, res) => {
     res.end(`Deleting campsite: ${req.params.campsiteId}`);
