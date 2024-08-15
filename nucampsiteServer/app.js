f5c1728d9f49602b98b2817b97ee790ff699af80
Ryan-GM <71759184+Ryan-GM@users.noreply.github.com>
2024-08-15 08:16:07 +0300

    Express Generator

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
+
 var app = express();
 
 app.use(logger('dev'));
@@ -16,5 +20,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 
 module.exports = app;
