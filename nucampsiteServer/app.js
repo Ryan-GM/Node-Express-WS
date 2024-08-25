db61ce30f6081cdc24865eab27431cd383af7781
Ryan-GM <71759184+Ryan-GM@users.noreply.github.com>
2024-08-25 20:47:14 +0300

    update mongoose user and commentSchema to support mongoose population

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -2,10 +2,11 @@ var express = require('express');
 var path = require('path');
 
 var logger = require('morgan');
-
+const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 const passport = require('passport');
 const authenticate = require('./authenticate');
+
 const config = require('./config');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- nucampsiteServer/models/user.js
+++ nucampsiteServer/models/user.js
@@ -5,7 +5,15 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
-   
+
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
     admin: {
         type: Boolean,
         default: false
