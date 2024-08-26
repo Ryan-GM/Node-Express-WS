0b300156e5e8d308a077c835cd9d7abffe3a2963
Ryan-GM <71759184+Ryan-GM@users.noreply.github.com>
2024-08-26 12:43:06 +0300

    remove sessions

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -2,12 +2,11 @@ var express = require('express');
 var path = require('path');
 
 var logger = require('morgan');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
-const passport = require('passport');
-const authenticate = require('./authenticate');
 
+const passport = require('passport');
 const config = require('./config');
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
@@ -18,7 +17,9 @@ const partnerRouter = require('./routes/partnerRouter');
 const mongoose = require('mongoose');
 
 const url = config.mongoUrl;
-const connect = mongoose.connect(url, {});
+const connect = mongoose.connect(url, {
+   
+});
 
 
 connect.then(() => {
@@ -32,25 +33,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
 
-app.use(session({
-    name: 'session-id',
-    secret: '12345-67890-09876-54321',
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore()
-}));
+
 
 app.use(passport.initialize());
-app.use(passport.session());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
