b07ee44e1b03cc164ac119533b37429c5ac2ff56
Ryan-GM <71759184+Ryan-GM@users.noreply.github.com>
2024-08-26 07:34:24 +0300

    create and export verifyAdmin

--- nucampsiteServer/authenticate.js
+++ nucampsiteServer/authenticate.js
@@ -8,15 +8,18 @@ const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
 const config = require('./config.js');
 
+// Local Strategy for username and password auth
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// Generate JWT token
 exports.getToken = function (user) {
     return jwt.sign(user, config.secretKey,
         {expiresIn: 3600});
 };
 
+// JWT strategy for token auth
 const opts = {};
 opts.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
@@ -37,5 +40,16 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
     }
 )
 );
-
+// Middleware to verify the user
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+// Middleware to verify admin privileges
+exports.verifyAdmin = (req, res, next) => {
+    if(req.user && req.user.admin) {
+        return next(); // proceed if user is admin
+    } else {
+        const err = new Error('You are not authorized to perform this operation!');
+        err.status = 403;
+        return next(err); // send error response if user not an admin
+    }
+};
