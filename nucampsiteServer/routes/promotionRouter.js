fb5b12f2feb7890711847ece8aa02b4f5b1621ae
Ryan-GM <71759184+Ryan-GM@users.noreply.github.com>
2024-08-26 12:39:02 +0300

    setup verifyAdmin middleware

--- nucampsiteServer/routes/promotionRouter.js
+++ nucampsiteServer/routes/promotionRouter.js
@@ -30,49 +30,49 @@ promotionRouter.route('/')
     res.end('PUT operation not supported on /promotions');
     })
     .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    Promotion.deleteMany()
+        Promotion.deleteMany()
+        .then(response => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(response);
+        })
+        .catch(err => next(err));
+    });
+
+// New promotionRouter.route()
+promotionRouter.route('/:promotionId')
+.get((req, res, next) => {
+    Promotion.findById(req.params.promotionId)
+    .then(promotion => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+    })
+    .catch(err => next(err));
+})
+.post(authenticate.verifyUser, (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /promotions/${req.params.promotionId}`);
+})
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Promotion.findByIdAndUpdate(req.params.promotionId, {
+        $set: req.body
+    }, { new: true })
+    .then(promotion => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+    })
+    .catch(err => next(err));
+})
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Promotion.findByIdAndDelete(req.params.promotionId)
     .then(response => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(response);
     })
     .catch(err => next(err));
-    });
-
-// New promotionRouter.route()
-promotionRouter.route('/:promotionId')
-.get((req, res, next) => {
-Promotion.findById(req.params.promotionId)
-.then(promotion => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json(promotion);
-})
-.catch(err => next(err));
-})
-.post(authenticate.verifyUser, (req, res) => {
-res.statusCode = 403;
-res.end(`POST operation not supported on /promotions/${req.params.promotionId}`);
-})
-.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-Promotion.findByIdAndUpdate(req.params.promotionId, {
-    $set: req.body
-}, { new: true })
-.then(promotion => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json(promotion);
-})
-.catch(err => next(err));
-})
-.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-Promotion.findByIdAndDelete(req.params.promotionId)
-.then(response => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json(response);
-})
-.catch(err => next(err));
 });
 
 module.exports = promotionRouter;
