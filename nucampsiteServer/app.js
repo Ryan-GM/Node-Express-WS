24b1306d3d739c0f6cc575cff37312ea662d54a4
Ryan-GM <71759184+Ryan-GM@users.noreply.github.com>
2024-09-03 10:04:45 +0300

    integrate favoriteRouter

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -13,6 +13,7 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
 
 
 const mongoose = require('mongoose');
@@ -63,5 +64,6 @@ app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
 
 module.exports = app;
